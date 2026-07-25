48385dfc6efebf5e1d4e54713da31f2aa7f37ef9
Allen Alcabaza <allen2talcabaza@gmail.com>
2026-07-26 06:10:43 +0800

    initial commit: folder structure

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+items = []
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return {"item": item, "message": "Item created successfully"}
