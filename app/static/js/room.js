1b75eaadf821ff870cc9d789b3f95109b59c697a
Allen Alcabaza <allen2talcabaza@gmail.com>
2026-07-28 22:55:53 +0800

    feat: Room Creation Modal

--- app/static/js/room.js
+++ app/static/js/room.js
@@ -1,8 +1,15 @@
 import {create_room} from "./api.js";
 
 const button = document.getElementById("create-room-button");
+const modal = document.getElementById("room-modal");
 
 button.addEventListener("click", async () => {
     const room = await create_room();
-    console.log(room);
+    
+    document.getElementById("room-id").textContent = room.room_code;
+    modal.style.display = "block";  
+});
+
+document.getElementById("close-modal-button").addEventListener("click", () => {
+    modal.style.display = "none";
 });
