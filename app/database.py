76953776bc7c5ecdcb79df15286763d34e5298ae
Allen Alcabaza <allen2talcabaza@gmail.com>
2026-09-06 00:18:39 +0800

    feat: Configure database for test deployment

--- app/database.py
+++ app/database.py
@@ -1,7 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/farframe"
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{db_user}:{db_password}"
+    f"@{db_host}:{db_port}/{db_name}"
+)
+
 
 engine = create_engine(DATABASE_URL)
 
